66b828492d425a6d1ffd752c02495c1d806e3843
Stegi56 <91616550+Stegi56@users.noreply.github.com>
2025-10-31 16:58:41 +0000

    Fix build 2

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import type { Experience } from "../data/profile";
+import type { Experience } from "../typeDefs/profile";
 
 import TechPills from "./TechPills";
 interface TechProps{
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import type { Project } from "../data/profile";
+import type { Project } from "../typeDefs/profile";
 
 import TechPills from "./TechPills";
 
